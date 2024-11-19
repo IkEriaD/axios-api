1aa5d64387a548e6a473dc34f0ecfeb88a8ebb20
Max-matix <125517747+Max-matix@users.noreply.github.com>
2024-11-19 17:28:40 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react';
+// import axios from 'axios';
+import axios from './axios';
 
 function App() {
+  const [products, setProducts] = useState([]);
+  const [error, setError] = useState([""]);
+  useEffect(() => {
+    getProducts();
+
+    // axios.get('/products')
+    // .then((response) => setProducts(response.data.products))
+    // // catch method runs only when theres an error in then method 
+    // .catch((error) => setError(error.message));
+  }, [])
+
+  // TO USE ASYNC METHOD : BEST TO USE..
+  async function getProducts() {
+    try {
+      const response = await axios.get('/products');
+      console.log(response);
+      setProducts(response.data.products);
+    } catch (error) {
+      setError(error.message)
+    }
+    
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {error !== "" && error}
+      {products.map((product, index) => {
+        return (
+          <div>
+            <h3 key={index}>{product.title}</h3>
+            <h4>${product.price}</h4>
+          </div>
+        )
+      })}
     </div>
   );
 }
 
 export default App;
+
+// IT CAN BE USE LIKE THIS : // axios.get('https://dummyjson.com/products')
+// OR CREATE A NEW Axios.JS AND IMPORT LIKE THIS:    // axios.get('/products')
